346e86015b681bf2f8b6b912f4d0e5a42b06c9fc
Mirzo_Uzoqov <60997768+Mirzo001@users.noreply.github.com>
2022-08-11 20:50:43 +0500

    init

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['167.99.213.89']
+ALLOWED_HOSTS = ['167.99.213.89','localhost','rohat-dent.uz','www.rohat-dent.uz']
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
